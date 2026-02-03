010f9295748c99bdeed95005e3a2e0d0240f9ff9
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2026-02-03 10:57:23 +0400

    [RT-5626] Cleaned up test.

--- tests/unit/DadataSuggestionsServiceTest.php
+++ tests/unit/DadataSuggestionsServiceTest.php
@@ -2,6 +2,9 @@
 
 namespace unit;
 
+use DadataSuggestions\DadataSuggestionsService;
+use DadataSuggestions\Data\Address;
+use DadataSuggestions\Response;
 use PHPUnit\Framework\TestCase;
 
 class DadataSuggestionsServiceTest extends TestCase
@@ -11,17 +14,13 @@ class DadataSuggestionsServiceTest extends TestCase
         $service = $this->getService();
         $response = $service->suggestAddress('moskv');
         $this->assertNotEmpty($response);
-        $this->assertInstanceOf(\DadataSuggestions\Response::class, $response);
+        $this->assertInstanceOf(Response::class, $response);
         $this->assertNotEmpty($response->getSuggestions());
         $this->assertIsArray($response->getSuggestions());
         foreach ($response->getSuggestions() as $suggestion) {
-            //$this->assertEquals('г Москва', $suggestion->getValue());
-            //$this->assertEquals('г Москва', $suggestion->getUnrestrictedValue());
-            /** @var \DadataSuggestions\Data\Address $data */
+            /** @var Address $data */
             $data = $suggestion->getData();
-            $this->assertInstanceOf(\DadataSuggestions\Data\Address::class, $data);
-            //$this->assertEquals('Россия', $data->country);
-            //$this->assertEquals('Москва', $data->city);
+            $this->assertInstanceOf(Address::class, $data);
         }
     }
 
@@ -30,30 +29,28 @@ class DadataSuggestionsServiceTest extends TestCase
         $service = $this->getService();
         $response = $service->suggestAddress('мск балтийская 6к1 5');
         $this->assertNotEmpty($response);
-        $this->assertInstanceOf(\DadataSuggestions\Response::class, $response);
+        $this->assertInstanceOf(Response::class, $response);
         $this->assertNotEmpty($response->getSuggestions());
         $this->assertIsArray($response->getSuggestions());
         foreach ($response->getSuggestions() as $suggestion) {
-            //$this->assertEquals('г Москва, ул Балтийская, д 6 к 1, кв 5', $suggestion->getValue());
-            //$this->assertEquals('г Москва, ул Балтийская, д 6 к 1, кв 5', $suggestion->getUnrestrictedValue());
-            /** @var \DadataSuggestions\Data\Address $data */
+            /** @var Address $data */
             $data = $suggestion->getData();
-            $this->assertInstanceOf(\DadataSuggestions\Data\Address::class, $data);
-            //$this->assertEquals('Россия', $data->country);
-            //$this->assertEquals('Москва', $data->city);
-            //$this->assertEquals('ул Балтийская', $data->street_with_type);
-            //$this->assertEquals('6', $data->house);
-            //$this->assertEquals('1', $data->block);
-            //$this->assertEquals('5', $data->flat);
+            $this->assertInstanceOf(Address::class, $data);
+            $this->assertNotEmpty($data->country);
+            $this->assertNotEmpty($data->city);
+            $this->assertNotEmpty($data->street_with_type);
+            $this->assertNotEmpty($data->house);
+            $this->assertNotEmpty($data->block);
+            $this->assertNotEmpty($data->flat);
         }
     }
 
     /**
-     * @return \DadataSuggestions\DadataSuggestionsService
+     * @return DadataSuggestionsService
      */
     protected function getService()
     {
-        $service = new \DadataSuggestions\DadataSuggestionsService();
+        $service = new DadataSuggestionsService();
         $service->setToken('ab626ac37b47868748ea2f408293ed6a1e420944');
         return $service;
     }
